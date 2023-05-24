299dcf17e199c2c4d8cc9c46a015dd28a63eabd7
igallion <82122432+igallion@users.noreply.github.com>
2023-05-24 17:00:37 -0400

    Changes lambda func back to original

--- lambda_function.py
+++ lambda_function.py
@@ -1,17 +1,13 @@
 import json
 import boto3
-import botocore
-
-client = boto3.client('dynamodb')
-dynamoTable = 'CloudResumeVisitorsTerraform'
 
 
-def increment_count():
+def lambda_handler(event, context):
     # TODO implement
     print('Incrementing visitor count for resume site now')
     client = boto3.client('dynamodb')
     response = client.update_item(
-        TableName=dynamoTable,
+        TableName='CloudResumeVisitorsTerraform',
         Key={
             'stat': {
                 'S': 'view-count',
@@ -33,35 +29,3 @@ def increment_count():
         },
         'body': json.dumps(response)
     }
-
-
-def initialize_Table():
-    response = client.put_item(
-        TableName=dynamoTable,
-        Item={
-            'stat': {'S': 'view-count'},
-            'Quantity': {'N': '1'}
-        },
-        ConditionExpression='attribute_not_exists(stat) AND attribute_not_exists(Quantity)'
-    )
-    return {
-        'statusCode': 200,
-        'headers': {
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'OPTIONS,GET'
-        },
-        'body': json.dumps(response)
-    }
-
-
-try:
-    initialize_Table()
-except botocore.exceptions.ClientError as e:
-    # Ignore the ConditionalCheckFailedException, bubble up
-    # other exceptions.
-    if e.response['Error']['Code'] != 'ConditionalCheckFailedException':
-        raise
-    else:
-        print("Table already initialized, incrementing count")
-        increment_count()
